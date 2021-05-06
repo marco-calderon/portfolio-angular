d35168734466c23343b6e21fae4a2d20b8aa1957
flexocarpius <flexocarpius@gmail.com>
2021-05-05 21:08:57 -0700

    Added toolbar and home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
